b5873002ca71941d95381961e0c3c54e3e7cc864
adonahy <adonahyskate@gmail.com>
2017-07-07 17:50:09 -0600

    Modificiaciones

--- connection.php
+++ connection.php
@@ -1,13 +1,13 @@
 <?php
     //$con = mysql_connect("sql200.eshost.com.ar","eshos_19125861","sanjose1234");
     $con = mysql_connect("localhost","root","");
-	
+
 	if (!$con)
 	{
 		die('Could not connect: ' . mysql_error());
 	}
 
-	mysql_select_db("syz_academia", $con);
+	mysql_select_db("SAF", $con);
     //mysql_select_db("eshos_19125861_aca", $con);
 
     $errors = array();
--- side_bar.php
+++ side_bar.php
@@ -14,13 +14,13 @@ $ct         =   0;
 $v          =   $r100['gastos'];
 $v1         =   $r100['gastos_adicionales'];
 $v2         =   $r100['gastos_academia'];
-/*$v3         =   $r100['ventas_pedido'];
-$v4         =   $r100['ventas_pfinal'];
+$v3         =   $r100['ventas_pedido'];
+/*$v4         =   $r100['ventas_pfinal'];
 $v5         =   $r100['ventas_reportes'];*/
 $vt         =   0;
-/*$compras    =   $r100['compras'];
+$compras    =   $r100['compras'];
 $compras1   =   $r100['proveedores'];
-$comprast   =   0;*/
+$comprast   =   0;
 $admin      =   $r100['administracion'];
 $admin1     =   $r100['admin_c_u'];
 $admin2     =   $r100['admin_e_u'];
@@ -38,44 +38,22 @@ echo '
         <ul class="nav nav-tabs nav-stacked main-menu">
         ';
     if ($c > 0){ 
-        $ct=$c1+$c2+$c3+$c4 ;
+        $ct=$c1+$c3 ;
         echo '
             <li>
-                <a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet"> Alumnos</span><span class="label label-important"> '.$ct.' </span></a>
+                <a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet"> Productos</span></a>
                 <ul>
                 ';
             if($c1 > 0){
                 echo '
-                    <li><a class="submenu" href="inscrip.php"><i class="icon-file-alt"></i><span class="hidden-tablet">Inscripciones</span></a></li>';
+                    <li><a class="submenu" href="inscrip.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Inventario</span></a></li>';
             }
-            if($c2 > 0){
-                echo'
-                    <li><a class="submenu" href="pago_alumno.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Pagos</span></a></li>';
-            }
+            
             if($c3 > 0){    
         echo'
-                    <li><a class="submenu" href="manto_cxc.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Tareas</span></a></li>';
+                    <li><a class="submenu" href="manto_cxc.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Proveedores</span></a></li>';
             }
-            if($c4 > 0){
-                echo'
-                    <li><a class="submenu" href="lista_alumno.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Lista de Alumnos</span></a></li>';
-            }
-           /* if($c5 > 0){
-                echo'
-                    <li><a class="submenu" href="#"><i class="icon-file-alt"></i><span class="hidden-tablet"> Facturación</span></a></li>';
-            }
-            if($c6 > 0){
-                echo'
-		            <li><a class="submenu" href="manto_gastos_adici.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Gastos adicionales</span></a></li>';
-			}
-            if($c7 > 0){
-                echo'
-                    <li><a class="submenu" href="reporte_conta.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Reportes</span></a></li>';
-            }
-            if($c8 > 0){
-                echo'
-                    <li><a class="submenu" href="reportes_comisiones.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Reportes Comisiones</span></a></li>';
-            }*/
+            
         echo'
                 </ul>	
             </li>
@@ -84,33 +62,33 @@ echo '
 	
 	
     if ($v > 0){ 
-        $vt=$v1+$v2;
+        $vt=$v1+$v2+$v3;
     echo '
             <li>
-				<a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet"> Gastos</span><span class="label label-important"> '.$vt.' </span></a>
+				<a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet"> Ventas</span></a>
 				<ul>';
         if($v1 > 0){
             echo'
-				    <li><a class="submenu" href="manto_gastos_adici.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Gastos Adicionales</span></a></li>';
+				    <li><a class="submenu" href="manto_gastos_adici.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Pre-Orden de compras</span></a></li>';
         }
         if($v2 > 0){
             echo'
-				    <li><a class="submenu" href="manto_gastos_acad.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Gastos Academia</span></a></li>';
+				    <li><a class="submenu" href="manto_gastos_acad.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Orden de compras</span></a></li>';
         }
        
-        /*if($v3 > 0){
+        if($v3 > 0){
             echo'
                 
-                    <li><a class="submenu" href="manto_pedidos.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Pedidos</span></a></li>
+                    <li><a class="submenu" href="manto_pedidos.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Facturación</span></a></li>
                     ';
 		}
         if($v4 > 0){
             echo'
-                    <li><a class="submenu" href="manto_pedidos_finales.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Pedidos Finales</span></a></li>
+                    <li><a class="submenu" href="manto_pedidos_finales.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Clientes</span></a></li>
                     ';
         }
         
-        if($v5 > 0){
+        /*if($v5 > 0){
             echo'
                     <li><a class="submenu" href="reportes_ventas.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Reportes</span></a></li>
                     ';
@@ -123,15 +101,27 @@ echo '
     }
 	
 	
-   /* if ($compras > 0){ 
+  /* if ($compras > 0){ 
         $comprast=$compras1;
     echo'
             <li>
-				<a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet"> Compras</span><span class="label label-important"> '.$comprast.' </span></a>
+				<a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet"> Pagos</span><span class="label label-important"> '.$comprast.' </span></a>
 				<ul>';
         if($compras1 > 0){
             echo'
-                    <li><a class="submenu" href="manto_proveedores.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Mantenimiento de proveedores</span></a></li>';
+                    <li><a class="submenu" href="manto_proveedores.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Proveedores</span></a></li>';
+        }
+       if($compras2 > 0){
+            echo'
+                    <li><a class="submenu" href="manto_proveedores.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Servicios</span></a></li>';
+        }
+       if($compras3 > 0){
+            echo'
+                    <li><a class="submenu" href="manto_proveedores.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Cuentas</span></a></li>';
+        }
+       if($compras4 > 0){
+            echo'
+                    <li><a class="submenu" href="manto_proveedores.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Planillas</span></a></li>';
         }
        
         echo'
@@ -145,7 +135,7 @@ if ($admin > 0){
         $admint=$admin1+$admin2+$admin3+$admin4+$admin5;
     echo '
             <li>
-                <a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet"> Administración</span><span class="label label-important"> '.$admint.' </span></a>
+                <a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet"> Administración</span></a>
 				<ul>';
     if($admin1 > 0){
             echo'
@@ -161,7 +151,7 @@ if ($admin > 0){
                     <li><a class="submenu" href="manto_roles.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Asignación de roles</span></a></li>
                     ';
     }
-    if($admin4 > 0){
+    /*if($admin4 > 0){
             echo'
                     <!--<li><a class="submenu" href="logs.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Bancos</span></a></li>-->
                     <li><a class="submenu" href="manto_bancos.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Bancos</span></a></li>
@@ -170,7 +160,7 @@ if ($admin > 0){
     if($admin5 > 0){
             echo'
                     <li><a class="submenu" href="reporte_conta.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Reportes</span></a></li>';
-    }
+    }*/
     echo'
 				</ul>	
             </li>';
